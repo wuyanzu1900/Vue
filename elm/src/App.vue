06dd1233c8a941576c31c871f87198c526ace9fa
zhuming <1109282075@qq.com>
2020-09-01 15:29:18 +0800

    饿了么

---
+++ elm/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <transition name="router-fade" mode="out-in">
+      <keep-alive>
+        <router-view v-if="$route.meta.keepAlive">
+        </router-view>
+      </keep-alive>
+    </transition>
+    <transition name="router-fade" maode="out-in">
+    <router-view v-if="!$router.meta.keepAlive"></router-view>
+    </transition>
+    <svg-icon></svg-icon>
+  </div>
+</template>
