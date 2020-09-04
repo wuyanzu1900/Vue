3f53bb0c3aaf5b315a8725d9428d369b615f16a3
zhuming <1109282075@qq.com>
2020-09-04 09:56:19 +0800

    el-badge源码

--- hello-world/src/components/HelloWorld.vue
+++ hello-world/src/components/HelloWorld.vue
@@ -1,61 +1,37 @@
 <template>
   <div class="hello">
-    <h1 class="tit">{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <el-badge :value="12" class="item" :max="9" :hidden="ishidden">
+      <el-button size="small">评论</el-button>
+    </el-badge>
+    <!-- <el-badge is-dot class="item">数据查询</el-badge>
+    <el-badge is-dot class="item">
+      <el-button class="share-button" icon="el-icon-share" type="primary"></el-button>
+    </el-badge>
+    <el-badge value="new" class="item">
+      <el-button size="small">评论</el-button>
+    </el-badge>
+    <el-badge value="hot" class="item">
+      <el-button size="small">回复</el-button>
+    </el-badge> -->
   </div>
 </template>
 
 <script>
+import IdBadge from '@/components/idBadge'
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "HelloWorld",
+  data(){
+    return{
+      msg:"欢迎吴彦祖"
+    }
+  },
+
+  component:{
+    IdBadge
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-.tit{
-  font-size: 24px;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
---
+++ hello-world/src/components/idBadge.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="el-badge">
+      <slot></slot>
+      <transition>
+          <sup v-show="hidden" v-text="content" class="el-badge_content">
+              
+          </sup>
+
+      </transition>
+      
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'IdBadge' ,
+    props:{
+        // 通用性更好
+
+        value:[String,Number],
+        max:Number,
+        isDot:Boolean,
+        hidden:Boolean,
+        type: {
+            type:String,
+            vaildator(val){
+                return ['primary','success','warning','info','danger'].indexOf(val) > -1
+            }
+        },
+        computed: {
+            content(){
+                if(this.isDot)return;
+                const value = this.value;
+                // 框架通用性之外,可配置性强
+                const max = this.max;
+
+                // console.log(typeof value,typeof max)
+                if(typeof value ==='number' && typeof max === 'number'){
+                    return value > max   ? `${max}+`:value;
+                }
+
+                return value
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
